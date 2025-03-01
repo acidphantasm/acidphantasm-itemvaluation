c069b2694d8b3217fd04930a9c1ea81c1fea4272
acidphantasm <acidphantasm@acidphantasm.com>
2025-03-01 00:41:43 -0600

    all the things

--- src/mod.ts
+++ src/mod.ts
@@ -1,64 +1,269 @@
 import { DependencyContainer } from "tsyringe";
 
-import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { IPostDBLoadModAsync } from "@spt/models/external/IPostDBLoadModAsync";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
-import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 import { ItemHelper } from "@spt/helpers/ItemHelper";
 import { BaseClasses } from "@spt/models/enums/BaseClasses";
+import { RagfairServerHelper } from "@spt/helpers/RagfairServerHelper";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { PreSptModLoader } from "@spt/loaders/PreSptModLoader";
+import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
+import { IRagfairConfig } from "@spt/models/spt/config/IRagfairConfig";
+import { ConfigServer } from "@spt/servers/ConfigServer";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
 
-class Mod implements IPostDBLoadMod
+class ItemValuation implements IPreSptLoadMod, IPostDBLoadModAsync
 {
-    public postDBLoad(container: DependencyContainer): void
+    private static container: DependencyContainer;
+    private static rairaiAmmoStats: boolean;
+    private static modConfig: Config = require("../config/config.json");
+    private static localeTable;
+    private static originalLocaleTable;
+    private static originalPriceTable;
+    private static updateTimer: NodeJS.Timeout;
+    private static nextUpdate: number = 0;
+    private static updateNumber: number = 0;
+    private static fs = require("fs");
+
+    public preSptLoad(container: DependencyContainer): void 
     {
-        // get database from server
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const preSptModLoader: PreSptModLoader = container.resolve<PreSptModLoader>("PreSptModLoader");
+        const logger = container.resolve<ILogger>("WinstonLogger");
+        ItemValuation.rairaiAmmoStats = preSptModLoader.getImportedModsNames().includes("rairaitheraichu-ammostats");
 
-        // Get all the in-memory json found in /assets/database
-        const tables: IDatabaseTables = databaseServer.getTables();
-
-        // ---------------------------------------------------------
-        // example #1
-        // Make the LEDX item sellable on flea market
-
-        // Find the ledx item by its Id
-        const ledx = tables.templates.items["5c0530ee86f774697952d952"];
-
-        // Update one of its properties to be true
-        ledx._props.CanSellOnRagfair = true;
-
-        // ---------------------------------------------------------
-        // example #2
-        // Get globals settings and set flea market min level to be 1
-        tables.globals.config.RagFair.minUserLevel = 1;
-
-        // ---------------------------------------------------------
-        // Example #3
-        // Loop over all magazines and make them weigh nothing
-
-        // Get ItemHelper ready to use
-        const itemHelper: ItemHelper = container.resolve<ItemHelper>("ItemHelper");
-
-        // Get all items in the database as an array so we can loop over them later
-        // tables.templates.items is a dictionary, the key being the items template id, the value being the objects data,
-        // we want to convert it into an array so we can loop over all the items easily
-        // Object.values lets us grab the 'value' part as an array and ignore the 'key' part
-        const items = Object.values(tables.templates.items);
-
-        // Use the itemHelper class to assist us in getting only magazines
-        // We are filtering all items to only those with a base class of MAGAZINE (5448bc234bdc2d3c308b4569)
-        const magazines = items.filter(x => itemHelper.isOfBaseclass(x._id, BaseClasses.MAGAZINE));
-
-        // Loop over all the magazines the above code found
-        for (const magazine of magazines)
+        if (!ItemValuation.isColourConverterInstalled())
         {
-            // Check the magazine has a weight property before we edit it
-            if (magazine._props.Weight)
+            logger.error("ColorConverterAPI not found. [Item Valuation] not loaded.");
+            return;
+        }
+    }
+
+    public async postDBLoadAsync(container: DependencyContainer): Promise<void>
+    {
+        ItemValuation.container = container;
+        ItemValuation.originalPriceTable = container.resolve<DatabaseServer>("DatabaseServer").getTables().templates.prices;
+        ItemValuation.originalLocaleTable = container.resolve<DatabaseServer>("DatabaseServer").getTables().locales.global;
+
+        // Update prices on startup
+        const currentTime = Math.floor(Date.now() / 1000);
+        let updateColours = false;
+        let firstUpdate = false;
+        if (currentTime > ItemValuation.nextUpdate)
+        {
+            updateColours = true;
+            firstUpdate = true;
+        }
+
+        if (!await ItemValuation.setPriceColouration(updateColours, firstUpdate))
+        {
+            console.log("Update failed")
+            return;
+        }
+
+        ItemValuation.updateTimer = setInterval(ItemValuation.setPriceColouration, (60 * 60.1 * 1000));
+    }
+
+    static async setPriceColouration(updateColours = true, firstUpdate = false): Promise<boolean>
+    {
+        const start = performance.now();
+
+        const databaseServer = ItemValuation.container.resolve<DatabaseServer>("DatabaseServer");
+        const ragfairServerHelper = ItemValuation.container.resolve<RagfairServerHelper>("RagfairServerHelper");
+        const ragfairConfig = ItemValuation.container.resolve<ConfigServer>("ConfigServer").getConfig(ConfigTypes.RAGFAIR) as IRagfairConfig;
+        const itemHelper = ItemValuation.container.resolve<ItemHelper>("ItemHelper");
+        ItemValuation.localeTable = databaseServer.getTables().locales.global;
+        const itemTable = databaseServer.getTables().templates.items;
+        const priceTable = databaseServer.getTables().templates.prices;
+        const handbookTable = databaseServer.getTables().templates.handbook.Items;
+
+        if (updateColours)
+        {
+            ItemValuation.nextUpdate = Math.floor(Date.now() / 1000) + 3606;
+            console.log("[ItemValuation] Updating Items" + ` ${firstUpdate ? "for the first time" : ""}`);
+            ItemValuation.updateNumber++
+        }
+        for (const item in itemTable)
+        {
+            if (!firstUpdate && ItemValuation.originalPriceTable[item] == priceTable[item]) continue;
+
+            // Get item details
+            const itemDetails = itemHelper.getItem(item);
+
+            // If item is ammo, and ammo stats is installed, skip
+            if (ItemValuation.rairaiAmmoStats && itemHelper.isOfBaseclass(itemDetails[1]._id, BaseClasses.AMMO)) continue;
+
+            // Get price, if not found - use handbook
+            let price = priceTable[item];
+            const itemHandbookPrice = handbookTable.find((handbookItem) => handbookItem.Id === item)?.Price ?? 0
+            if (!price)
             {
-                // Set its weight to 0
-                magazine._props.Weight = 0;
+                price = itemHandbookPrice;
             }
+
+            
+
+            // Check if item's baseclass is in the unreasonable mod prices, and adjust it's price accordingly for colouration
+            for (const baseClass in ragfairConfig.dynamic.unreasonableModPrices)
+            {
+                if (itemHelper.isOfBaseclass(itemDetails[1]._id, baseClass))
+                {
+                    if (price > itemHandbookPrice * ragfairConfig.dynamic.unreasonableModPrices[baseClass].handbookPriceOverMultiplier) 
+                    {
+                        price = itemHandbookPrice * ragfairConfig.dynamic.unreasonableModPrices[baseClass].newPriceHandbookMultiplier;
+                    }
+                }
+            }
+
+            // Get item height & width, skip if not found, calculate the price per slot
+            const height = itemTable[item]?._props?.Height;
+            const width = itemTable[item]?._props?.Width;
+            if (!height || !width) continue;
+            const pricePerSlot = Math.round(price / (height * width));
+
+            // Check if item is valid for flea
+            const validFleaItem = ragfairServerHelper.isItemValidRagfairItem(itemDetails);
+
+            // Set background colour depending on baseclass
+            let newBackgroundColour;
+            let descriptionPrice;
+            let addDescription = false;
+            let perSlotDescription = false;
+
+            if (itemHelper.isOfBaseclass(itemDetails[1]._id, BaseClasses.WEAPON) && ItemValuation.modConfig.colourWeapons)
+            {
+                newBackgroundColour = ItemValuation.getWeaponColour(price, validFleaItem);
+                descriptionPrice = price;
+                addDescription = true;
+            } 
+            else if (itemHelper.isOfBaseclass(itemDetails[1]._id, BaseClasses.AMMO) && ItemValuation.modConfig.colourAmmo)
+            {
+                newBackgroundColour = ItemValuation.getAmmoColour(price, validFleaItem);
+                descriptionPrice = price;
+                addDescription = true;
+            }
+            else if (itemHelper.isOfBaseclass(itemDetails[1]._id, BaseClasses.MONEY))
+            {
+                newBackgroundColour = "#000000";
+            }
+            else if (ItemValuation.modConfig.colourNormalItems) 
+            {
+                newBackgroundColour = ItemValuation.getItemColour(pricePerSlot, validFleaItem);
+                descriptionPrice = pricePerSlot;
+                addDescription = true;
+                perSlotDescription = true;
+            }
+            else continue;
+
+            if (addDescription) ItemValuation.addPriceToLocales(descriptionPrice, validFleaItem, itemDetails[1]._id, perSlotDescription);
+            itemTable[item]._props.BackgroundColor = newBackgroundColour;
+        }
+        const timeTaken = performance.now() - start;
+        console.log(`[ItemValuation] Update took ${timeTaken.toFixed(2)}`)
+        return true;
+    }
+    private static getItemColour(pricePerSlot: number, availableOnFlea: boolean): string
+    {
+        if (ItemValuation.modConfig.colourFleaBannedItems && !availableOnFlea) return ItemValuation.modConfig.fleaBannedColour;
+        if (pricePerSlot < ItemValuation.modConfig.badItemPerSlotMaxValue) return ItemValuation.modConfig.badColour;
+        if (pricePerSlot < ItemValuation.modConfig.poorItemPerSlotMaxValue) return ItemValuation.modConfig.poorColour;
+        if (pricePerSlot < ItemValuation.modConfig.fairItemPerSlotMaxValue) return ItemValuation.modConfig.fairColour;
+        if (pricePerSlot < ItemValuation.modConfig.goodItemPerSlotMaxValue) return ItemValuation.modConfig.goodColour;
+        if (pricePerSlot < ItemValuation.modConfig.veryGoodItemPerSlotMaxValue) return ItemValuation.modConfig.veryGoodColour;
+        return ItemValuation.modConfig.exceptionalColour;
+    }
+
+    private static getWeaponColour(price: number, availableOnFlea: boolean): string
+    {
+        if (ItemValuation.modConfig.colourFleaBannedItems && !availableOnFlea) return ItemValuation.modConfig.fleaBannedColour;
+        if (price < ItemValuation.modConfig.badWeaponMaxValue) return ItemValuation.modConfig.badColour;
+        if (price < ItemValuation.modConfig.poorWeaponMaxValue) return ItemValuation.modConfig.poorColour;
+        if (price < ItemValuation.modConfig.fairWeaponMaxValue) return ItemValuation.modConfig.fairColour;
+        if (price < ItemValuation.modConfig.goodWeaponMaxValue) return ItemValuation.modConfig.goodColour;
+        if (price < ItemValuation.modConfig.veryGoodWeaponMaxValue) return ItemValuation.modConfig.veryGoodColour;
+        return ItemValuation.modConfig.exceptionalColour;
+    }
+
+    private static getAmmoColour(price: number, availableOnFlea: boolean): string
+    {
+        if (ItemValuation.modConfig.colourFleaBannedItems && !availableOnFlea) return ItemValuation.modConfig.fleaBannedColour;
+        if (price < ItemValuation.modConfig.badAmmoMaxValue) return ItemValuation.modConfig.badColour;
+        if (price < ItemValuation.modConfig.poorAmmoMaxValue) return ItemValuation.modConfig.poorColour;
+        if (price < ItemValuation.modConfig.fairAmmoMaxValue) return ItemValuation.modConfig.fairColour;
+        if (price < ItemValuation.modConfig.goodAmmoMaxValue) return ItemValuation.modConfig.goodColour;
+        if (price < ItemValuation.modConfig.veryGoodAmmoMaxValue) return ItemValuation.modConfig.veryGoodColour;
+        return ItemValuation.modConfig.exceptionalColour;
+    }
+
+    private static addPriceToLocales(price: number, availableOnFlea: boolean, itemID: string, perSlotDescription = false): void
+    {
+        for (const locale in ItemValuation.localeTable)
+        {
+            const priceType = perSlotDescription ? "Price Per Slot:" : "Price:"
+            const originalDescription = ItemValuation.originalLocaleTable[locale][`${itemID} Description`]; 
+            ItemValuation.localeTable[locale][`${itemID} Description`] = `${priceType} ${ItemValuation.formatToRoubles(price)} | Flea Banned: ${availableOnFlea ? "No" : "Yes"} | Update: ${ItemValuation.updateNumber}\n\n` + originalDescription;
+        }
+    }
+
+    private static formatToRoubles(amount: number): string 
+    {
+        return new Intl.NumberFormat("ru-RU", {
+            style: "currency",
+            currency: "RUB",
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        }).format(amount);
+    }
+
+    private static isColourConverterInstalled(): boolean 
+    {
+        const pluginName = "rairai.colorconverterapi.dll";
+        // Fails if there's no ./BepInEx/plugins/ folder
+        try 
+        {
+            const pluginList = ItemValuation.fs.readdirSync("./BepInEx/plugins").map(plugin => plugin.toLowerCase());
+            return pluginList.includes(pluginName);
+        }
+        catch 
+        {
+            return false;
         }
     }
 }
 
-export const mod = new Mod();
+interface Config
+{
+    colourFleaBannedItems: boolean,
+    colourNormalItems: boolean,
+    colourWeapons: boolean,
+    colourAmmo: boolean,
+    
+    badItemPerSlotMaxValue: number,
+    badWeaponMaxValue: number,
+    badAmmoMaxValue: number,
+    badColour: string,
+    
+    poorItemPerSlotMaxValue: number,
+    poorWeaponMaxValue: number,
+    poorAmmoMaxValue: number,
+    poorColour: string,
+
+    fairItemPerSlotMaxValue: number,
+    fairWeaponMaxValue: number,
+    fairAmmoMaxValue: number,
+    fairColour: string,
+
+    goodItemPerSlotMaxValue: number,
+    goodWeaponMaxValue: number,
+    goodAmmoMaxValue: number,
+    goodColour: string,
+
+    veryGoodItemPerSlotMaxValue: number,
+    veryGoodWeaponMaxValue: number,
+    veryGoodAmmoMaxValue: number,
+    veryGoodColour: string,
+
+    exceptionalColour: string,
+    fleaBannedColour: string,
+}
+
+export const mod = new ItemValuation();
